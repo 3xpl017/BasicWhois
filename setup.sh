c6085d26857b77438ba32b7969aa3c0f9d1bcc1e
Phoenix <153451582+3xpl017@users.noreply.github.com>
2024-01-14 12:17:20 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,3 +2,13 @@ clear
 echo ------------------------------------------------------------------------------------------
 echo [~] Instalando requerimientos...
 echo ------------------------------------------------------------------------------------------
+sudo su
+sudo apt-get update
+sudo apt-get install python3
+sudo apt-get install python3-pip
+pip3 install os
+pip3 install colarama
+pip3 install sys
+pip3 install time
+pip3 install socket
+echo [~] Requerimientos instalados. Usa el comando python3 main.py para inciar BasicWhois.
